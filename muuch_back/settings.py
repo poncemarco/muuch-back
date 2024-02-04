a2cb7a87611ffda3131e9a35850b0177076b152b
Marco Ponce <marco_cap@192.168.1.4>
2024-02-04 14:49:36 -0600

    dir files changed

--- muuch_back/settings.py
+++ muuch_back/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 
 STATIC_URL = '/assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"),)
 
 
 MEDIA_URL = "/media/"
